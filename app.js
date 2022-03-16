9fcf3fb93413ccb470721b8ab1fedd225c9b0235
THOMAS HARDING <hardingt604@gmail.com>
2022-03-16 10:07:23 -0600

    added required html elements and comment layout for presumed required js elements

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 // import functions and grab DOM elements
+// grab each button
+// grab each input
+// grab current polls div
+// grab past polls div
+// grab all current poll display bits: title+curr votes for a and b
 
 // let state
+let aVotes = 0;
+let bVotes = 0;
+let pastPolls = [];
+
+function displayCurrentPoll() {
+  //display state of current poll to current poll div
+}
+
+function displayAllPolls() {
+  //clear dom
+  //for each poll in past polls
+    //renderPoll(poll)
+    //append to past polls div
+}
+
+function renderPoll(poll) {
+    return Node;
+    //return dom node containing poll information to be displayed
+    //one node for the title of the poll
+    //two nodes, one for a and b
+      //each has children of a title and a number of votes in its own node
+}
 
 // set event listeners 
+//start poll click event listener should submit input-poll form element
+//eventlistener to increment  or decrement each vote on appropriate buttons
+//eventlistener on close poll button to render current poll, push to pastpolls, run displayallpolls(), clear current poll div
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
