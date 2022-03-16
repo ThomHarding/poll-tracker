51d9e9438e893d8f3ef873644c0d41c0ed5aeead
THOMAS HARDING <hardingt604@gmail.com>
2022-03-16 10:57:39 -0600

    all event listeners are written in some state, still need displaycurrentpoll and renderpoll

--- app.js
+++ app.js
@@ -1,24 +1,84 @@
 // import functions and grab DOM elements
-// grab each button
-// grab each input
-// grab current polls div
-// grab past polls div
-// grab all current poll display bits: title+curr votes for a and b
+let addToAButton = document.getElementById('option-a-add');
+let undoAButton = document.getElementById('option-a-undo');
+let addToBButton = document.getElementById('option-b-add');
+let undoBButton = document.getElementById('option-b-undo');
+let closePollButton = document.getElementById('close-poll');
+let inputForm = document.querySelector('form');
+let pollInput = document.getElementById('poll-title-input');
+let optionAInput = document.getElementById('input-option-a');
+let optionBInput = document.getElementById('input-option-b');
+let currentPollTitle = document.getElementById('current-question');
+let currentATitleDiv = document.getElementById('option-a-title');
+let currentAVotes = document.getElementById('option-a-votes');
+let currentBTitleDiv = document.getElementById('option-b-title');
+let currentBVotes = document.getElementById('option-b-votes');
+let currentPollDiv = document.getElementById('current-poll');
+let pastPollDiv = document.getElementById('past-polls');
 
 // let state
+let currentQuestion;
+let currentATitle;
+let currentBTitle;
 let aVotes = 0;
 let bVotes = 0;
 let pastPolls = [];
+
+addToAButton.addEventListener('click', () => {
+    aVotes++;
+    displayCurrentPoll();
+});
+
+undoAButton.addEventListener('click', () => {
+    aVotes--;
+    displayCurrentPoll();
+});
+
+addToBButton.addEventListener('click', () => {
+    bVotes++;
+    displayCurrentPoll();
+});
+
+undoBButton.addEventListener('click', () => {
+    bVotes--;
+    displayCurrentPoll();
+});
+
+inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let data = new FormData(inputForm);
+    currentQuestion = data.get('poll=title-input');
+    currentATitle = data.get('input-option-a');
+    currentBTitle = data.get('input-option-b');
+    displayCurrentPoll();
+});
+
+closePollButton.addEventListener('click', () => {
+    //update each global variable up there to what's actually in the input
+    let pollObject = {
+        question: currentQuestion,
+        optionATitle: currentATitle,
+        optionBTitle: currentBTitle,
+        optionAVotes: aVotes,
+        optionBVotes: bVotes,
+    };
+    //render current poll, push to pastpolls
+    pastPolls.push(renderPoll(pollObject));
+    inputForm.reset();
+    displayAllPolls();
+    currentPollDiv.innerHTML = '';
+});
 
 function displayCurrentPoll() {
   //display state of current poll to current poll div
 }
 
 function displayAllPolls() {
-  //clear dom
-  //for each poll in past polls
-    //renderPoll(poll)
-    //append to past polls div
+    pastPollDiv.innerHTML = '';
+    for (let poll of pastPolls) {
+        let renderedPoll = renderPoll(poll);
+        pastPollDiv.appendChild(renderedPoll);
+    }
 }
 
 function renderPoll(poll) {
@@ -28,12 +88,3 @@ function renderPoll(poll) {
     //two nodes, one for a and b
       //each has children of a title and a number of votes in its own node
 }
-
-// set event listeners 
-//start poll click event listener should submit input-poll form element
-//eventlistener to increment  or decrement each vote on appropriate buttons
-//eventlistener on close poll button to render current poll, push to pastpolls, run displayallpolls(), clear current poll div
-
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
